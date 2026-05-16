24cc97927fd2821519bc982b9c686586cf8e4b92
Adam Carnall <adam@Mac.fritz.box>
2026-05-16 10:30:12 +0100

    incorporated feedback from Ross

--- check_photoz/compare_cosmos_2020_2025_photoz.py
+++ check_photoz/compare_cosmos_2020_2025_photoz.py
@@ -7,16 +7,20 @@ from astropy.coordinates import SkyCoord, match_coordinates_sky
 from astropy import units as u
 
 
-def positional_cross_match(cat1, cat2, dist_arcsec):
+def positional_cross_match(cat1, cat2, dist_arcsec, how="inner", ra_col_1="ra",
+                           dec_col_1="dec", ra_col_2="ra", dec_col_2="dec",
+                           suffix1="_1", suffix2="_2"):
     """
     Cross-match two catalogues based on ra and dec columns, returning
     a new joined catalogue with only sources that have a close match.
+    Reproduces functionality of TOPCAT tool's sky match algorithm with
+    match seclection: "best match, symmetric" and join type: "1 and 2"
     """
 
-    coords1 = SkyCoord(ra=cat1['ra'].values*u.degree,
-                       dec=cat1['dec'].values*u.degree)
-    coords2 = SkyCoord(ra=cat2['ra'].values*u.degree,
-                       dec=cat2['dec'].values*u.degree)
+    coords1 = SkyCoord(ra=cat1[ra_col_1].values*u.degree,
+                       dec=cat1[dec_col_1].values*u.degree)
+    coords2 = SkyCoord(ra=cat2[ra_col_2].values*u.degree,
+                       dec=cat2[dec_col_2].values*u.degree)
 
     result = match_coordinates_sky(coords1, coords2)
 
@@ -24,8 +28,14 @@ def positional_cross_match(cat1, cat2, dist_arcsec):
 
     matched = pd.merge(cat1[dist_mask].reset_index(drop=True),
                        cat2.iloc[result[0][dist_mask]].reset_index(drop=True),
-                       left_index=True, right_index=True,
-                       suffixes=('_1', '_2'))
+                       left_index=True, right_index=True, how=how,
+                       suffixes=(suffix1, suffix2))
+
+    matched["cat2_row"] = result[0][dist_mask]
+    matched["match_sep_arcsec"] = result[1][dist_mask]
+
+    idxmin = matched.groupby("cat2_row")["match_sep_arcsec"].idxmin()
+    matched = matched.loc[idxmin].reset_index(drop=True)
 
     return matched
 
@@ -34,15 +44,17 @@ def positional_cross_match(cat1, cat2, dist_arcsec):
 
 # COSMOS2020 catalogue, unedited download from
 # https://irsa.ipac.caltech.edu/data/COSMOS/tables/cosmos2020/
-cosmos2020 = Table.read("../COSMOS2020_CLASSIC_R1_v2.2_p3.fits").to_pandas()
+c2020_path = "../COSMOS2020_CLASSIC_R1_v2.2_p3.fits"
+cosmos2020_raw = Table.read(c2020_path).to_pandas()
 
-# Cuts to exactly reproduce Ross's 2023 sample selection that was used to
-# define objects Bagpipes was run on for UVJ colours and stellar masses
-cosmos2020 = cosmos2020[cosmos2020["FLAG_COMBINED"] == 0]
-cosmos2020 = cosmos2020[cosmos2020["UVISTA_H_MAG_APER3"] <= 24]
+cosmos2020_raw.rename(columns={"ALPHA_J2000": "ra",
+                               "DELTA_J2000": "dec"}, inplace=True)
 
-cosmos2020.rename(columns={"ALPHA_J2000": "ra",
-                           "DELTA_J2000": "dec"}, inplace=True)
+# Cut by photometry flag and H-band magnitude to match MOONRISE criteria
+mask_phot_combined = (cosmos2020_raw["FLAG_COMBINED"] == 0)
+mask_h24 = (cosmos2020_raw["UVISTA_H_MAG_APER3"] <= 24)
+both_mask = mask_h24 & mask_phot_combined
+cosmos2020 = cosmos2020_raw[both_mask].reset_index(drop=True)
 
 # COSMOS2025 photometric catalogue, unedited download from
 # https://cosmos2025.iap.fr/catalog_download.php
@@ -68,20 +80,27 @@ cosmos2025_fluxes = cosmos2025_fluxes.to_pandas()
 c2025_photoz_path = "../COSMOSWeb_mastercatalog_v1.1_lephare.fits"
 cosmos2025_photoz = Table.read(c2025_photoz_path).to_pandas()
 
-cosmos2025 = pd.merge(cosmos2025_fluxes, cosmos2025_photoz, left_index=True,
-                      right_index=True)
+cosmos2025_raw = pd.merge(cosmos2025_fluxes, cosmos2025_photoz,
+                          left_index=True, right_index=True)
 
-cosmos2025 = cosmos2025[cosmos2025["mag_auto_f150w"] < 24]
-cosmos2025 = cosmos2025[cosmos2025["zfinal"] > 0]
+# Cut both catalogues to a common set of objects
+cosmos2020 = positional_cross_match(cosmos2020, cosmos2025_raw, 0.5,
+                                    suffix1="", suffix2="_cosmos2025")
+
+cosmos2025 = positional_cross_match(cosmos2025_raw, cosmos2020, 0.5,
+                                    suffix1="", suffix2="_cosmos2020")
 
 # DJA v4.5 NIRSpec catalogue, unedited csv download from
 # https://s3.amazonaws.com/msaexp-nirspec/extractions/nirspec_public_v4.5.html
 dja_cat = pd.read_csv("../dja_nirspec_v4.5.csv")
 dja_cat = dja_cat[dja_cat["grade"] == 3]
 
-# ##### Compare photozs to DJA v4.5 speczs #####
-cosmos2020_dja = positional_cross_match(cosmos2020, dja_cat, 0.35)
-cosmos2025_dja = positional_cross_match(cosmos2025, dja_cat, 0.35)
+# ##### Compare photozs to DJA v4.5 specz compilation #####
+
+cosmos2020_dja = positional_cross_match(cosmos2020, dja_cat, 0.3)
+cosmos2025_dja = positional_cross_match(cosmos2025, dja_cat, 0.3,
+                                        ra_col_1="ra_cosmos2020",
+                                        dec_col_1="dec_cosmos2020")
 
 moonrise_zphot_mask_cosmos2020 = ((cosmos2020_dja["ez_z_phot"] >= 0.7)
                                   & (cosmos2020_dja["ez_z_phot"] <= 2.6))
@@ -95,6 +114,7 @@ moonrise_zdja_mask_cosmos2020 = ((cosmos2020_dja["zfit"] >= 0.7)
 moonrise_zdja_mask_cosmos2025 = ((cosmos2025_dja["zfit"] >= 0.7)
                                  & (cosmos2025_dja["zfit"] <= 2.6))
 
+# Make plot
 fig = plt.figure(figsize=(12, 5))
 gs = fig.add_gridspec(1, 2)
 ax1 = fig.add_subplot(gs[0, 0])
@@ -154,7 +174,7 @@ ax2.annotate("$z_\\mathrm{phot}$ wrongly outside 0.7$-$2.6 range: "
              xy=(0.05, 0.85), xycoords="axes fraction", fontsize=14,
              ha="left", va="top")
 
-plt.savefig("cosmos2020_2025_dja_comparison.pdf", dpi=300,
+plt.savefig("cosmos2020_2025_photozs_vs_dja_v4.5_speczs.pdf", dpi=300,
             bbox_inches="tight")
 
 # ##### Now compare photozs to Khostovan et al. (2025) specz compilation #####
@@ -166,22 +186,28 @@ khost = Table.read(specz_path).to_pandas()
 khost.rename(columns={"specz": "specz_khost", "ra_corrected": "ra",
                       "dec_corrected": "dec"}, inplace=True)
 
-khost = khost[khost["Confidence_level"] >= 97]
+# Cut Khostovan catalogue to objects that have counterparts in both cosmos2020
+# and cosmos2025 catalogues, as well as high confidence speczs (flags 3 and 4)
+both_cats_mask = (khost["Id_COS20_Classic"] > 0) & (khost["Id_COSMOS25"] > 0)
+khost = khost[both_cats_mask]
 
-cosmos2020_khost = pd.merge(cosmos2020, khost, left_on="ID",
+khost = khost[khost["Confidence_level"] >= 95]
+
+# Match using cosmos2020 and cosmos2025 IDs provided by Khostovan et al.
+cosmos2020_khost = pd.merge(cosmos2020_raw, khost, left_on="ID",
                             right_on="Id_COS20_Classic",
                             how="inner", suffixes=("_1", "_2"))
 
-cosmos2025_khost = pd.merge(cosmos2025, khost, left_on="id",
+cosmos2025_khost = pd.merge(cosmos2025_raw, khost, left_on="id",
                             right_on="Id_COSMOS25",
                             how="inner", suffixes=("_1", "_2"))
-
+"""
 # Alternatively could do our own matching, Khostovan uses a 1.5" radius
 # for COSMOS2020 but must use a smaller one for COSMOS2025 though exact
 # value not clear - I've tested different radii, makes little difference
-# cosmos2020_khost = positional_cross_match(cosmos2020, khost, 1.0)
-# cosmos2025_khost = positional_cross_match(cosmos2025, khost, 1.0)
-
+cosmos2020_khost = positional_cross_match(cosmos2020, khost, 0.5)
+cosmos2025_khost = positional_cross_match(cosmos2025, khost, 0.5)
+"""
 moonrise_zphot_mask_cosmos2020 = ((cosmos2020_khost["ez_z_phot"] >= 0.7)
                                   & (cosmos2020_khost["ez_z_phot"] <= 2.6))
 
@@ -194,6 +220,7 @@ moonrise_zkhost_mask_cosmos2020 = ((cosmos2020_khost["specz_khost"] >= 0.7)
 moonrise_zkhost_mask_cosmos2025 = ((cosmos2025_khost["specz_khost"] >= 0.7)
                                    & (cosmos2025_khost["specz_khost"] <= 2.6))
 
+# Make plot
 fig = plt.figure(figsize=(12, 5))
 gs = fig.add_gridspec(1, 2)
 ax1 = fig.add_subplot(gs[0, 0])
@@ -253,5 +280,5 @@ ax2.annotate("$z_\\mathrm{phot}$" + f" wrongly outside 0.7$-$2.6 range: "
              xy=(0.05, 0.85), xycoords="axes fraction", fontsize=14,
              ha="left", va="top")
 
-plt.savefig("cosmos2020_2025_khostovan25_comparison.pdf", dpi=300,
+plt.savefig("cosmos2020_2025_photozs_vs_khostovan25_v1.1_speczs.pdf", dpi=300,
             bbox_inches="tight")
